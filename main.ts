75500aa1adaf55c27b84b5ddcfccd536942d62a4
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-05-21 22:58:53 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,95 @@
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    if (receivedString == "B") {
+        etat_suivant = 1
+    } else if (receivedString == "R" && etat == 9) {
+        etat_suivant = 2
+    } else if (receivedString == "S") {
+        etat_suivant = 3
+    } else if (receivedString == "A") {
+        etat_suivant = 4
+    }
 })
+let etat_suivant = 0
+let etat = 0
 radio.setGroup(1)
+etat = 0
+etat_suivant = 0
 basic.showIcon(IconNames.SmallDiamond)
 basic.forever(function () {
-	
+    etat = etat_suivant
+    if (etat == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.pause(500)
+        etat = 9
+    } else if (etat == 2) {
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        etat = 0
+    } else if (etat == 3) {
+        basic.showLeds(`
+            . . # . #
+            . . # . #
+            . . # . #
+            . . # . #
+            . . # . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (etat == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
