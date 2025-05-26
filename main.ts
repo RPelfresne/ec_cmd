b76f1aa6d711393044268046270dca6cf5236452
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-05-26 13:25:37 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedString(function (receivedString) {
+    msg = receivedString
+})
+let msg_lu = ""
+let msg = ""
+radio.setGroup(1)
+let etat_suivant = 0
+let etat = 0
 let nbLeds = 133
 let dureeMontee = 20000
 let strip = neopixel.create(DigitalPin.P1, nbLeds, NeoPixelMode.RGB)
@@ -10,29 +18,33 @@ basic.pause(100)
 strip.show()
 basic.pause(100)
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(200)
-    strip.show()
-    basic.pause(2000)
-    for (let index = 0; index < pauseEntreLeds; index++) {
-        strip.shift(1)
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(100)
-        strip.show()
-        basic.pause(100)
-    }
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.pause(200)
-    strip.show()
-    basic.pause(2000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(200)
-    strip.show()
-    basic.pause(2000)
-    for (let index = 0; index < 4; index++) {
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
-        basic.pause(100)
+    if (msg == "B") {
+        etat = etat_suivant
+        msg_lu = msg
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        basic.pause(100)
+        basic.pause(200)
+        strip.show()
+        basic.pause(2000)
+        for (let index = 0; index < pauseEntreLeds; index++) {
+            strip.shift(1)
+            strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+            basic.pause(100)
+            strip.show()
+            basic.pause(100)
+        }
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(200)
+        strip.show()
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(200)
+        strip.show()
+        basic.pause(2000)
+        for (let index = 0; index < 4; index++) {
+            strip.showColor(neopixel.colors(NeoPixelColors.White))
+            basic.pause(100)
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+            basic.pause(100)
+        }
     }
 })
