25f628c2de71f41bbe84fe4e021ae088a34feffd
RPelfresne <49885062+RPelfresne@users.noreply.github.com>
2025-07-06 22:30:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,8 +9,8 @@ let etat = 0
 let msg = ""
 radio.setGroup(1)
 let nbLeds = 133
-let dureeMontee = 10000
-let delai_rouge = 5000
+let dureeMontee = 11000
+let delai_rouge = 60000
 let debut_rouge = 0
 let strip = neopixel.create(DigitalPin.P1, nbLeds, NeoPixelMode.RGB)
 let pauseEntreLeds = dureeMontee / nbLeds
